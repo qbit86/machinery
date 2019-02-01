6366dc2e1acf57049178e5f3a2bb0ace6430e9a5
Viktor T <qbit86@gmail.com>
2019-02-01 03:14:36 +0300

    Implement StateMachine<>.

--- src/Machinery/IStatePolicy.cs
+++ src/Machinery/IStatePolicy.cs
@@ -4,7 +4,7 @@
     {
         bool TryCreateNewState(TContext context, TState currentState, TEvent ev, out TState newState);
         void OnExiting(TContext context, TState currentState, TEvent ev, TState newState);
-        void OnEntered(TContext context, TState oldState, TEvent ev, TState currentState);
-        void DisposeState(TContext context, TState oldState, TEvent ev);
+        void OnEntered(TContext context, TState currentState, TEvent ev, TState oldState);
+        void DisposeState(TContext context, TState currentState, TEvent ev, TState stateToDispose);
     }
 }
---
+++ src/Machinery/StateMachine.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Threading;
+
+namespace Machinery
+{
+    public sealed class StateMachine<TContext, TState, TEvent, TStatePolicy>
+    where TStatePolicy : IStatePolicy<TContext, TState, TEvent>
+    {
+        private TState _currentState;
+        private readonly TStatePolicy _statePolicy;
+        private int _locked;
+
+        public StateMachine(TState initialState, TStatePolicy statePolicy)
+        {
+            if (initialState == null)
+                throw new ArgumentNullException(nameof(initialState));
+
+            if (statePolicy == null)
+                throw new ArgumentNullException(nameof(statePolicy));
+
+            _currentState = initialState;
+            _statePolicy = statePolicy;
+        }
+
+        public TState CurrentState => _currentState;
+
+        public bool Process(TContext context, TEvent ev)
+        {
+            int locked = Interlocked.Exchange(ref _locked, 1);
+            if (locked == 1)
+                return false;
+
+            try
+            {
+                bool transit = _statePolicy.TryCreateNewState(context, _currentState, ev, out TState newState);
+                if (!transit)
+                    return true;
+
+                if (newState == null)
+                    throw new InvalidOperationException(nameof(newState));
+
+                try
+                {
+                    _statePolicy.OnExiting(context, _currentState, ev, newState);
+                }
+                catch
+                {
+                    _statePolicy.DisposeState(context, _currentState, ev, newState);
+                    throw;
+                }
+
+                TState oldState = _currentState;
+                _currentState = newState;
+
+                try
+                {
+                    _statePolicy.OnEntered(context, _currentState, ev, oldState);
+                }
+                finally
+                {
+                    _statePolicy.DisposeState(context, _currentState, ev, oldState);
+                }
+            }
+            finally
+            {
+                Interlocked.Exchange(ref _locked, 0);
+            }
+
+            return true;
+        }
+    }
+}
