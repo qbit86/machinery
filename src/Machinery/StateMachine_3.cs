59c4adc8fe247df493e3a53ed87d5b626c2c6eb8
Viktor T <qbit86@gmail.com>
2019-02-05 01:51:22 +0300

    Add StateMachine_3.

---
+++ src/Machinery/StateMachine_3.cs
@@ -0,0 +1,84 @@
+namespace Machinery
+{
+    using System;
+    using System.Threading;
+
+    public static class StateMachine<TEvent>
+    {
+#pragma warning disable CA1000 // Do not declare static members on generic types
+        public static StateMachine<TState, TEvent, TStatePolicy> Create<TState, TStatePolicy>(
+            TState initialState, TStatePolicy statePolicy)
+            where TStatePolicy : IStatePolicy<TState, TEvent>
+        {
+            return new StateMachine<TState, TEvent, TStatePolicy>(initialState, statePolicy);
+        }
+#pragma warning restore CA1000 // Do not declare static members on generic types
+    }
+
+    public sealed class StateMachine<TState, TEvent, TStatePolicy>
+        where TStatePolicy : IStatePolicy<TState, TEvent>
+    {
+        private readonly TStatePolicy _statePolicy;
+
+        private TState _currentState;
+        private int _lock;
+
+        public StateMachine(TState initialState, TStatePolicy statePolicy)
+        {
+            if (initialState == null)
+                throw new ArgumentNullException(nameof(initialState));
+
+            if (statePolicy == null)
+                throw new ArgumentNullException(nameof(statePolicy));
+
+            _currentState = initialState;
+            _statePolicy = statePolicy;
+        }
+
+        public TState CurrentState => _currentState;
+
+        public bool Process(TEvent ev)
+        {
+            if (Interlocked.Exchange(ref _lock, 1) == 1)
+                return false;
+
+            try
+            {
+                bool transit = _statePolicy.TryCreateNewState(_currentState, ev, out TState newState);
+                if (!transit)
+                    return true;
+
+                if (newState == null)
+                    throw new InvalidOperationException(nameof(newState));
+
+                try
+                {
+                    _statePolicy.OnExiting(_currentState, ev, newState);
+                }
+                catch
+                {
+                    _statePolicy.DisposeState(_currentState, ev, newState);
+                    throw;
+                }
+
+                TState oldState = _currentState;
+                _currentState = newState;
+
+                try
+                {
+                    _statePolicy.OnEntered(_currentState, ev, oldState);
+                }
+                finally
+                {
+                    _statePolicy.DisposeState(_currentState, ev, oldState);
+                }
+            }
+            finally
+            {
+                Interlocked.Exchange(ref _lock, 0);
+            }
+
+            return true;
+        }
+    }
+}
