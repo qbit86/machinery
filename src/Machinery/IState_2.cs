60f4673895866073873b82eb3f642fc81f52db20
Viktor T <qbit86@gmail.com>
2019-10-11 23:01:13 +0300

    Add IState`2

---
+++ src/Machinery/IState_2.cs
@@ -0,0 +1,9 @@
+namespace Machinery
+{
+    public interface IState<TContext, TEvent>
+    {
+        bool TryCreateNewState(TContext context, TEvent ev, out IState<TContext, TEvent> newState);
+        void OnExiting(TContext context, TEvent ev, IState<TContext, TEvent> newState);
+        void OnEntered(TContext context, TEvent ev, IState<TContext, TEvent> oldState);
+    }
+}
