8c3ba7aaa10b225d86d309cabf147bfea56d2bda
Viktor T <qbit86@gmail.com>
2019-10-15 01:25:11 +0300

    Add Machinery.State2Demo project

---
+++ samples/Machinery.State2Demo/Opened.cs
@@ -0,0 +1,27 @@
+namespace Machinery
+{
+    using System;
+
+    internal sealed class Opened : IState<Door, Event>
+    {
+        public bool TryCreateNewState(Door context, Event ev, out IState<Door, Event> newState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnExiting(Door context, Event ev, IState<Door, Event> newState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnRemain(Door context, Event ev, IState<Door, Event> currentState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnEntered(Door context, Event ev, IState<Door, Event> oldState)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ samples/Machinery.State2Demo/State2Demo.cs
@@ -0,0 +1,30 @@
+﻿namespace Machinery
+{
+    using System;
+    using System.IO;
+
+    internal enum Event
+    {
+        None = 0,
+        // The same event used for both opening and closing.
+        Interact,
+        Lock,
+        Unlock
+    }
+
+    internal sealed class Door
+    {
+        internal static Door Instance { get; } = new Door();
+
+        private Door() { }
+    }
+
+    internal static class State2Demo
+    {
+        private static TextWriter Out => Console.Out;
+
+        private static void Main()
+        {
+        }
+    }
+}
