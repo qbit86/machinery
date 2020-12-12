625758cf0c4e5c428f87f7633bde35990bf5277c
Viktor T <viktor.t@gmail.com>
2020-12-13 01:11:11 +0300

    Add NotNullWhen(true) and MaybeNullWhen(false) attributes under conditional compilation

--- src/Machinery/IState_2.cs
+++ src/Machinery/IState_2.cs
@@ -1,8 +1,17 @@
 namespace Machinery
 {
+#if NETSTANDARD2_1 || NETCOREAPP3_0
+    using System.Diagnostics.CodeAnalysis;
+
+#endif
+
     public interface IState<TContext, TEvent>
     {
+#if NETSTANDARD2_1 || NETCOREAPP3_0
+        bool TryCreateNewState(TContext context, TEvent ev, [NotNullWhen(true)] out IState<TContext, TEvent>? newState);
+#else
         bool TryCreateNewState(TContext context, TEvent ev, out IState<TContext, TEvent> newState);
+#endif
         void OnExiting(TContext context, TEvent ev, IState<TContext, TEvent> newState);
         void OnRemain(TContext context, TEvent ev);
         void OnEntered(TContext context, TEvent ev, IState<TContext, TEvent> oldState);
