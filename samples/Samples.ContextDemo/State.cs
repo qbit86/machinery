4f331b33e028f87ea2afc2d02692fe99e2cb1de2
Viktor Ptitselov <qbit86@gmail.com>
2023-10-05 02:07:46 +0300

    Cleanup sample

--- samples/Samples.ContextDemo/State.cs
+++ samples/Samples.ContextDemo/State.cs
@@ -5,12 +5,9 @@ using System.Diagnostics.CodeAnalysis;
 internal abstract class State : IState<Door, Event, State>
 {
     public abstract bool TryCreateNewState(Door context, Event ev, [NotNullWhen(true)] out State? newState);
-
     public abstract void OnExiting(Door context, Event ev, State newState);
     public void OnExited(Door context, Event ev, State newState) { }
-
     public abstract void OnRemain(Door context, Event ev);
     public void OnEntering(Door context, Event ev, State oldState) { }
-
     public abstract void OnEntered(Door context, Event ev, State oldState);
 }
